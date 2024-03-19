4215e7bcc717bf9965492f8db9569422904be486
Anton <antoncodes@outlook.com>
2024-03-19 12:11:45 +0200

    ProductCategoryTabs

--- src/app/(main)/collections/[slug]/page.tsx
+++ src/app/(main)/collections/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import CommonProductList2 from '@/Common/components/ProductList2';
+import ProductCategoryTabs from '@/Product/components/CategoryTabs';
 
 export default function RouteCollection() {
   return (
@@ -6,23 +7,8 @@ export default function RouteCollection() {
       <h2 className="text-2xl font-bold tracking-tight text-gray-900 mb-6">
         All products
       </h2>
-      <div className="flex gap-x-12 flex-wrap mb-6">
-        <a
-          href="#"
-          className="py-2 text-sm font-semibold leading-6 text-indigo-600"
-        >
-          All products
-        </a>
-        <a href="#" className="py-2 text-sm leading-6 text-gray-900 underline">
-          Electronics
-        </a>
-        <a href="#" className="py-2 text-sm leading-6 text-gray-900 underline">
-          Home &amp; Garden
-        </a>
-        <a href="#" className="py-2 text-sm leading-6 text-gray-900 underline">
-          Sports &amp; Outdoor
-        </a>
-      </div>
+      <ProductCategoryTabs />
+      <div className="mb-8" />
       <CommonProductList2 />
     </div>
   );
